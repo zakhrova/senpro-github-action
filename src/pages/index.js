567b7beb41fd274d707b3abf410ab3c5e269ca05
zakhrova <zakhrovasalsabila@mail.ugm.ac.id>
2026-09-04 10:21:05 +0700

    test: comment imports to fail build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+//import Head from 'next/head'
+//import Image from 'next/image'
+//import { Inter } from '@next/font/google'
+//import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [Nama] | [NIU]
+            Zakhrova Salsabila | 534625
           </h5>
         </div>
       </main>
